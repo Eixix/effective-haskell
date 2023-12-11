270c0c366b68f9e5fb4d38eeba4b2a1290f23d85
Eixix <tobiasbetz@hotmail.de>
2023-12-11 15:53:01 +0100

    Exercise 2

--- exercises/exercise2.hs
+++ exercises/exercise2.hs
@@ -10,7 +10,22 @@ reverseWithFoldl = foldl (flip (:)) []
 reverseWithFoldr :: Foldable t => t a -> [a]
 reverseWithFoldr = foldr (\x y -> y <> [x]) []
 
-zipWith' f (x:xs) (y:ys) = f x y <> zipWith' f xs ys
+zipWith' f [] _ = []
+zipWith' f _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 
-zipWith'' f xs ys = [f x y | x <- xs, y <- ys]
+zipWith'' f xs ys =
+  [f (xs !! idx) (ys !! idx) | idx <- [0 .. len - 1]]
+  where
+    len = min (length xs) (length ys)
 
+zipWithFoldl f as bs =
+  reverse $ fst results
+  where
+    results = foldl applyFunction ([], as) bs
+    applyFunction (zipped, []) _ = (zipped, [])
+    applyFunction (zipped, x:xs) val = (f x val : zipped, xs)
+
+concatMapFoldr f = foldl (\x y -> f x <> y) []
+
+concatMapFoldl f = foldl (\x y -> y <> f x) []
