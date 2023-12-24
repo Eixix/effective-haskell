1dea3cccc81ce2783b78df5ac919137333d69a08
Eixix <tobiasbetz@hotmail.de>
2023-12-24 13:40:02 +0100

    Peano

--- types.hs
+++ types.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE RecordWildCards #-}
-
 -- data CustomerInfo = CustomerInfo {firstName :: String, lastName :: String, widgetCount :: Int, money :: Int}
 
 -- showCustomer CustomerInfo {..} = firstName <> lastName <> show widgetCount <> show money
@@ -55,3 +53,65 @@ getPersonSalary person =
   case person of
     Employee employee -> SomeInt (employeeSalary employee)
     _ -> NoInt
+
+-- data Either a b = Left a | Right b
+eitherToMaybe :: Either a1 a2 -> Maybe a2
+eitherToMaybe e =
+  case e of
+    Left _ -> Nothing
+    Right val -> Just val
+
+data Peano = Z | S Peano
+
+toPeano :: Int -> Peano
+toPeano 0 = Z
+toPeano n = S (toPeano $ n - 1)
+
+fromPeano :: Peano -> Int
+fromPeano Z = 0
+fromPeano (S p) = succ (fromPeano p)
+
+eqPeano :: Peano -> Peano -> Bool
+eqPeano Z Z = True
+eqPeano (S n) (S p) = eqPeano n p
+eqPeano _ _ = False
+
+addPeano :: Peano -> Peano -> Peano
+addPeano Z n = n
+addPeano (S n) p = addPeano n (S p)
+
+data List a = Empty | Cons a (List a)
+
+toList :: [a] -> List a
+toList = foldr Cons Empty
+
+fromList :: List a -> [a]
+fromList = listFoldr (:) []
+
+listFoldr :: (a -> b -> b) -> b -> List a -> b
+listFoldr _ b Empty = b
+listFoldr f b (Cons x xs) = f x $ listFoldr f b xs
+
+listFoldl :: (b -> a -> b) -> b -> List a -> b
+listFoldl _ b Empty = b
+listFoldl f b (Cons x xs) = listFoldl f (f b x) xs
+
+listHead :: List a -> Maybe a
+listHead Empty = Nothing
+listHead (Cons x xs) = Just x
+
+listTail :: List a -> List a
+listTail Empty = Empty
+listTail (Cons x xs) = xs
+
+listReverse :: List a -> List a
+listReverse Empty = Empty
+listReverse (Cons x xs) = Cons (listHead xs) (Cons x Empty)
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x : xs) = reverse xs <> [x]
+
+listMap :: (a -> b) -> List a -> List b
+listMap _ Empty = Empty
+listMap f (Cons x xs) = Cons (f x) (listMap f xs)
